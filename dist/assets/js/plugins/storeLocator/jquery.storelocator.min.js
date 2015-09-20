644b3a3f4e78109f406e451632a8c1510f087f53
Bjorn Holine <bjorn2404@gmail.com>
2015-09-20 01:29:44 -0500

    Grunt build after updates

--- dist/assets/js/plugins/storeLocator/jquery.storelocator.min.js
+++ dist/assets/js/plugins/storeLocator/jquery.storelocator.min.js
@@ -1,4 +1,4 @@
-/*! jQuery Google Maps Store Locator - v2.0.9 - 2015-09-20
+/*! jQuery Google Maps Store Locator - v2.0.10 - 2015-09-20
 * http://www.bjornblog.com/web/jquery-store-locator-plugin
 * Copyright (c) 2015 Bjorn Holine; Licensed MIT */
 
