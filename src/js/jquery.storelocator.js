e09f1b280f091231b6bf09c5f6d0e3a53dfe2e32
Cristiano <drcomix@users.noreply.github.com>
2016-04-05 10:05:15 +0200

    prevents zoom reset while dragging the map

--- src/js/jquery.storelocator.js
+++ src/js/jquery.storelocator.js
@@ -1796,6 +1796,7 @@
 			// Determine the new origin addresss
 			var newAddress = new this.reverseGoogleGeocode(this);
 			newCenterCoords = new google.maps.LatLng(mappingObj.lat, mappingObj.lng);
+			newCenterCoords.setZoom(map.getZoom());
 			newAddress.geocode({'latLng': newCenterCoords}, function (data) {
 				if (data !== null) {
 					mappingObj.origin = addressInput = data.address;
